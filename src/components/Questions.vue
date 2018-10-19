4a7485727f50f55567d5b60949489912f7b1e84d
14nrv <contact@styro.fr>
2018-10-19 15:46:07 +0200

    feat: add question component

---
+++ src/components/Questions.vue
@@ -0,0 +1,59 @@
+<template>
+<div class="container">
+    <h2 class="display-4">Where am i ?</h2>
+    <div class="form-group">
+      <input type="text" name="title">
+    </div>
+
+    <button class="edit" @click="edit">Edit</button>
+    <div v-if="editing">
+        <form>
+            <div  class="form-group">
+               <input type="text" name="name">
+            </div>
+            <button class="update" @click="update">Update</button>
+            <button class="cancel">Cancel</button>
+        </form>
+    </div>
+
+    <ul v-show="false">
+        <li>Hidden</li>
+    </ul>
+
+    <button id="emit" @click="$emit('applied')">Emit Event</button>
+
+</div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    editing: false
+  }),
+  methods: {
+    update () {
+      this.$emit('applied')
+      this.editing = true
+    },
+    edit () {
+      this.$emit('isEditing', 40)
+      this.editing = true
+    },
+    fireStatus () {
+      this.$emit('status', 200, 300, 400, 404, 500)
+    },
+    fireObject () {
+      const data = {
+        company: 'Apple.inc',
+        product: 'iPhone X',
+        price: '1200 usd'
+      }
+      this.$emit('obj', data)
+    },
+    firePrices () {
+      const prices = [10, 12, 20, 40, 30]
+      this.$emit('prices', prices)
+    }
+  }
+}
+</script>
